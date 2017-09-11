c4f20a99bd7de90b9c14a05d233aeef3e398b5ad
Joe Lucero <jlucerond@gmail.com>
2017-09-11 10:31:22 -0600

    Add new Xcode project

---
+++ GreenBike/GreenBike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreenBike
+//
+//  Created by Joe Lucero on 9/11/17.
+//  Copyright © 2017 Joe Lucero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
